35d96b166da87d0964a028ab874e73cc46941bdb
unknown <naruputter@gmail.com>
2023-07-23 18:52:08 +0700

    readMIDI

--- readMIDI.py
+++ readMIDI.py
@@ -1,33 +1,121 @@
 import mido
+import matplotlib.pyplot as plt
 
-def convert_midi_value_to_mido_message(midi_value):
 
-	time = midi_value[-1]
-	midi_value.pop(-1)
+class MIDI :
+
+	def __init__(self, tick=480, bpm=30, filePath=None):
+
+		if filePath :
+
+			midiPack = read_midi_file(filePath)
+
+		else :
+
+			midiPack = []
+
+		self.file_path = filePath
+		self.midi_pack = midiPack
+		self.tick = tick
+		self.bpm = bpm
+
+
+
+
+### Manage Function #########
+
+
+def read_midi_file(filePath):
+
+	mido_message_pack = mido.MidiFile(filePath, clip=True)
+
+	midi_pack = convert_mido_pack_to_midi_pack(mido_message_pack)
+
+	return midi_pack
+
+
+def save_midi_file(midiPack, filePath):
+
+	mido_pack = None
+
+	if type(midiPack) == list :
+
+		mido_pack = convert_midi_pack_to_mido_pack(midiPack)
+
+	elif type(midiPack) == mido.midifiles.midifiles.MidiFile :
+
+		mido_pack = midiPack
+
+
+	if mido_pack :
+
+		mido_pack.save(filePath)
+
+		return f"sve file to {filePath} succesfull"
+
+	else :
+
+		return "value is not match to save midi"
+
+
+
+### Convert Function ########
+
+
+def convert_time_to_mido_format(time ,tick=480, bpm=30):
+
+	constant = ( tick * 60 ) / bpm 
+	mido_time = round(time*constant)
+
+	return mido_time
+
+
+def convert_midi_value_to_mido_message(midiValue, tick=480, bpm=30):
+
+	time = midiValue[-1]
+	midiValue.pop(-1)
 
 	try:
-		mido_message = mido.Message.from_bytes(midi_value)
-		mido_message.time = time
+		mido_message = mido.Message.from_bytes(midiValue)
+		mido_time = convert_time_to_mido_format(time, tick, bpm)
+		mido_message.time = mido_time 
 	except:
 		mido_message = None
 
 	return mido_message
 
-def convert_mido_message_to_midi_value(mido_message):
 
-	midi_value = mido_message.bytes()
-	midi_value.append(mido_message.time)
+def convert_mido_message_to_midi_value(midoMessage):
+
+	midi_value = midoMessage.bytes()
+	midi_value.append(midoMessage.time)
 
 	return midi_value
 
 
-def read_midi_file(filePath):
+def convert_midi_pack_to_mido_pack(midiPack, tick=480, bpm=30):
+
+	mido_pack = mido.MidiFile()
+	mido_track = mido.MidiTrack()
+
+	for midi_value in midiPack :
+
+		mido_message = convert_midi_value_to_mido_message(midi_value, tick, bpm)
+
+		if mido_message :
+
+			mido_track.append(mido_message)
+
+	mido_pack.tracks.append(mido_track)
+
+	return mido_pack
+
+
+def convert_mido_pack_to_midi_pack(midoPack):
 
 	midi_pack = []
 
-	mido_message_pack = mido.MidiFile(filePath, clip=True)
-
-	for mido_message in mido_message_pack :
+	for mido_message in midoPack :
 
 		midi_value = convert_mido_message_to_midi_value(mido_message)
 
@@ -36,26 +124,14 @@ def read_midi_file(filePath):
 	return midi_pack
 
 
-def convert_midi_pack_to_mido_pack(midi_pack):
-
-	mido_pack = mido.MidiFile()
-
-	for midi_value in midi_pack :
-
-		mido_message = convert_midi_value_to_mido_message(midi_value)
-
-		if mido_message :
-
-			mido_pack.tracks.append(mido_message)
-
-	return mido_pack
-
-
-
 
 
 	
 if __name__ == '__main__':
 	
-	midi = read_midi_file('file/midi.mid')
-	print(convert_midi_pack_to_mido_pack(midi))
+	midi = MIDI(filePath='test.mid')
+
+	print(midi.midi_pack)
+
+	print(convert_midi_pack_to_mido_pack(midi.midi_pack))
+
